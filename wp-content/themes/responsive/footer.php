ff70b9abf5d34ca092786f28f5360840d0c80a3f
megadog <megadog.su@gmail.com>
2014-06-28 11:39:03 -0500

    Fixed minify

--- wp-content/themes/responsive/footer.php
+++ wp-content/themes/responsive/footer.php
@@ -127,43 +127,14 @@ if ( !defined('ABSPATH')) exit;
 </div>
 <!-- end #footer -->
 
-<?php wp_footer(); ?>
 
-<!--justfont script below DO NOT REMOVE-->
-<script type="text/javascript">window.jfAsyncInit=function(){jf.main({'appId':'086f88d9VxjMD59pN8yHbN0ssBMY8uQQ5kOegP8zvjUNQ0elpJy7Mik3bDOFw9HBhDtIfKq6z-tebOLImgyNW8PYsHmaoQARrIOq0BXYKEmM4BIRcx0dk-HyJ32nnZ85i9k-YgQBKB7o8zd5kPkBF_LV6Ysd4qUWcUS0P2-fmyNPQFs3EPc=','p':'1845','font':{'wt005':{'css':{'0':'.ct3'}},'wt011':{'css':{'0':'.ct5'}},'wt064':{'css':{'0':'.ct4'}},'WCL-02':{'css':{'0':'.ct8'}},'wts43':{'css':{'0':'.ct9'}},'xingothic-tc-w4':{'css':{'0':'.ct7'},'alias':'xinheiw4'},'xingothic-tc-w8':{'css':{'0':'.ct2'},'alias':'xinheiw8'},'xingothic-tc-w6':{'css':{'0':'.ct6'},'alias':'xinheiw6'},'xingothic-tc-w3':{'css':{'0':'.ct1'},'alias':'xinheiw3'},'wts55':{'css':{'0':'.ct10'}},'datf5':{'css':{'0':'.datf5'},'alias':'datf5'},'datz5':{'css':{'0':'.datz5'},'alias':'dialoslab','english':'rockwell'},'dats4':{'css':{'0':'.dats4'},'alias':'dialosung','english':'baskerville'},'dass3':{'css':{'0':'.dass3'},'alias':'dass3'},'daty9':{'css':{'0':'.daty9'},'alias':'dialoroundultrabold','english':'Varela Round'}}})};(function(){var c={'scriptTimeout':3000};var h=document.getElementsByTagName("html")[0];h.className+=" jf-loading";var e=setTimeout(function(){h.className=h.className.replace(/(\s|^)jf-loading(\s|$)/g," ");h.className+=" jf-inactive"},c.scriptTimeout);var jfscript=document.createElement("script");var d=false;jfscript.type="text/javascript";jfscript.async=true;jfscript.src='http://ds.justfont.com/js/stable/v/1.13/';jfscript.onload=jfscript.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded"){return}d=true;clearTimeout(e)};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(jfscript,s)})();</script>
+<!-- WP-Minify JS -->
 <?php if(is_home()): ?>
 <script type="text/javascript">
    jQuery(function() {
 		jQuery('#cn-slideshow').slideshow();
-		
-		/*var refreshIntervalId;
-		refreshIntervalId =  setInterval(function() { 
-		
-			jQuery('#cn-slideshow a.cn-nav-next').trigger("click"); 
-		} , 4000);
-		
-		jQuery('div.cn-images').bind("mouseenter", (function(){ 
-			clearInterval(refreshIntervalId); 
-		}));
-		
-		jQuery('div.cn-images').bind("mouseleave", (function(){ 
-			
-			refreshIntervalId =  setInterval(function() { 
-				jQuery('#cn-slideshow a.cn-nav-next').trigger("click"); 
-			} , 4000);
-		}));
-		
-		jQuery("div.cn-bar").live("mouseenter", function() {
-			clearInterval(refreshIntervalId); 
-		});
-		
-		jQuery("div.cn-bar").live("mouseleave", function() {
-			refreshIntervalId =  setInterval(function() { 
-				jQuery('#cn-slideshow a.cn-nav-next').trigger("click"); 
-			} , 4000);
-		});*/
-	
 	});
 </script> 
 <?php endif;?>
+<?php wp_footer(); ?>
 </body></html>
--- wp-content/wp-cache-config.php
+++ wp-content/wp-cache-config.php
@@ -16,7 +16,7 @@ $cache_gc_email_me = 0; //Added by WP-Cache Manager
 $cache_scheduled_time = '00:00'; //Added by WP-Cache Manager
 $wp_cache_mobile_groups = ''; //Added by WP-Cache Manager
 $wp_cache_mobile_prefixes = 'w3c , w3c-, acs-, alav, alca, amoi, audi, avan, benq, bird, blac, blaz, brew, cell, cldc, cmd-, dang, doco, eric, hipt, htc_, inno, ipaq, ipod, jigs, kddi, keji, leno, lg-c, lg-d, lg-g, lge-, lg/u, maui, maxo, midp, mits, mmef, mobi, mot-, moto, mwbp, nec-, newt, noki, palm, pana, pant, phil, play, port, prox, qwap, sage, sams, sany, sch-, sec-, send, seri, sgh-, shar, sie-, siem, smal, smar, sony, sph-, symb, t-mo, teli, tim-, tosh, tsm-, upg1, upsi, vk-v, voda, wap-, wapa, wapi, wapp, wapr, webc, winw, winw, xda , xda-'; //Added by WP-Cache Manager
-$wp_cache_refresh_single_only = '1'; //Added by WP-Cache Manager
+$wp_cache_refresh_single_only = '0'; //Added by WP-Cache Manager
 $wp_cache_make_known_anon = 0; //Added by WP-Cache Manager
 $wp_cache_mod_rewrite = 1; //Added by WP-Cache Manager
 $wp_cache_front_page_checks = 0; //Added by WP-Cache Manager
@@ -31,8 +31,8 @@ $wp_cache_slash_check = 0; //Added by WP-Cache Manager
 if ( ! defined('WPCACHEHOME') )
 	define( 'WPCACHEHOME', WP_CONTENT_DIR . "/plugins/wp-super-cache/" ); //Added by WP-Cache Manager
 
-$cache_compression = 1; //Added by WP-Cache Manager
-$cache_enabled = true; //Added by WP-Cache Manager
+$cache_compression = 0; //Added by WP-Cache Manager
+$cache_enabled = false; //Added by WP-Cache Manager
 $super_cache_enabled = true; //Added by WP-Cache Manager
 $cache_max_time = 1800; //Added by WP-Cache Manager
 //$use_flock = true; // Set it true or false if you know what to use
@@ -46,7 +46,7 @@ $cache_acceptable_files = array( 'wp-comments-popup.php', 'wp-links-opml.php', '
 $cache_rejected_uri = array('wp-.*\\.php', 'index\\.php');
 $cache_rejected_user_agent = array ( 0 => 'bot', 1 => 'ia_archive', 2 => 'slurp', 3 => 'crawl', 4 => 'spider', 5 => 'Yandex' );
 
-$cache_rebuild_files = 1; //Added by WP-Cache Manager
+$cache_rebuild_files = 0; //Added by WP-Cache Manager
 
 // Disable the file locking system.
 // If you are experiencing problems with clearing or creating cache files
@@ -97,9 +97,9 @@ $wp_cache_pages[ "pages" ] = 0;
 $wp_cache_pages[ "single" ] = 0;
 $wp_cache_pages[ "author" ] = 0;
 $wp_cache_hide_donation = 0;
-$wp_cache_not_logged_in = 1; //Added by WP-Cache Manager
+$wp_cache_not_logged_in = 0; //Added by WP-Cache Manager
 $wp_cache_clear_on_post_edit = 0; //Added by WP-Cache Manager
 $wp_cache_hello_world = 0; //Added by WP-Cache Manager
-$wp_cache_mobile_enabled = 1; //Added by WP-Cache Manager
+$wp_cache_mobile_enabled = 0; //Added by WP-Cache Manager
 $wp_cache_cron_check = 1; //Added by WP-Cache Manager
 ?>
